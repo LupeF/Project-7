e7b44ff5ebafa02c4038ca2e4dbdbcece4a88afa
LupeF <46634202+LupeF@users.noreply.github.com>
2021-03-07 11:37:33 -0800

    add keyframe to nav bell, and JS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
+//*notifications//
+const notification = document.getElementById('notifications');
 
 
+//*creates Alertbanner// 
 const alertbanner = document.getElementById('alert');
-//cr eating the html for the banner
 alertbanner.innerHTML = `
 <div class="alert-banner">
-<p><strong>Alert: </strong> You have <strong>6</strong> overdue tasks to complete!</p>
+<p> <strong> Alert: </strong> You have <strong>6</strong> overdue tasks to complete!</p>
 </div>
 <p class="alert-banner-close"> x </p>
 `;
@@ -14,116 +16,130 @@ alertbanner.addEventListener('click', e =>{
         alertbanner.style.display="none";
     }
 });
-//////line graph chart//////
-// const trafficCanvas = document.getElementById('traffic-chart').getContext('2d');
-// ​let​ trafficData = {
-//     labels: [​"16-22"​, ​"23-29"​, ​"30-5"​, ​"6-12"​, ​"13-19"​, ​"20-26"​, ​"27-3"​,
-//     "4-10"​, ​"11-17"​, ​"18-24"​, ​"25-31"​], 
-//     datasets: [{
-//     data: [​750​, ​1250​, ​1000​, ​2000​, ​1500​, ​1750​, ​1250​, ​1850​, ​2250​, ​1500​, 2500​],
-//     backgroundColor: ​'rgba(116, 119, 191, .3)'​,
-//     borderWidth: ​1​, }]
-//     };
-// let​ trafficOptions = { 
-//         aspectRatio: ​2.5​, 
-//         animation: {
-//         duration: ​0 },
-//           scales: {
-//             yAxes: [{
-//         ticks: {
-//         beginAtZero:​true }
-//         }] },
-//         legend : {
-//         display: ​false }
-//         };
-// ​let​ trafficChart = ​new​ Chart(trafficCanvas, { 
-//         type: ​'line'​,
-//         data: trafficData,
-//         options: trafficOptions
-//         });        
-var ctx = document.getElementById('traffic-chart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
+
+//* line-chart//
+const trafficCanvas = document.getElementById('traffic-chart');
+let trafficData = {
+    labels: ["1:00",
+             "2:00",
+             "3:00",
+             "4:00",
+             "5:00",
+             "6:00",
+             "7:00",
+             "8:00",
+             "9:00",
+             "10:00",
+             "11:00"],
+    datasets: [
+        {
+            data: [0, 100, 300, 48, 100, 250, 450,380,550,500,370],
+            backgroundColor: 'rgba(116,119,191,.3)',
+            borderWidth:1,
+        }
+    ]
+}
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    },
+    legend: {
+        display: false
+    }
+};
+let trafficChart = new Chart(trafficCanvas,{
     type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['16-22',
-                 '23-29', 
-                 '10-5', 
-                 '6-12', 
-                 '13-19', 
-                 '20-26', 
-                 '27-3', 
-                 '4-10',
-                 '11-17',
-                 '18-24',
-                 '25-31'],
-        datasets: [{
-            
-            backgroundColor: 'rgba(116, 119, 191, .3',
-            borderWidth: 1,
-            data: [{
-                x:100,
-                y:500},{
-                x:10,
-                y:1000},{
-                x:5,
-                y:1500},{
-                x:2,
-                y:2000},{
-                x:20,
-                y:2500    
-                }
-            ]
-            // data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
+    data: trafficData,
+    options: trafficOptions
 });
 
-///////bar graph////////
-var ctx = document.getElementById('daily-chart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
+//* bar graph//
+const dailyCanvas = document.getElementById('daily-chart');
+let dailyData = {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets: [
+        {
+        label: '# of Hits',
+        backgroundColor: ' #7477bf',
+        borderWidth: 1,
+        data: [7, 115, 175, 125, 225, 200, 100]
+        }
+    ]
+};
+const dailyOptions = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    },
+    legend: {
+        display: false
+    }
+}
+
+let dailyChart = new Chart(dailyCanvas,{
     type: 'bar',
-
-
-    // The data for our dataset
-    data: {
-        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: ' #bdcddd',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
+    data: dailyData,
+    options: dailyOptions
 });
 
-////donut graph
-var ctx = document.getElementById('mobile-users').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'doughnut',
-
-
-    // The data for our dataset
-    data: {
-        labels: ['Phones', 'Tablets', 'Desktop',],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: ' #bdcddd',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+//*Donut-graph//
+const mobileCanvas = document.getElementById('mobile-users');
+let mobileData = {
+    labels: ['Phones', 'Tablets', 'Desktop',],
+    datasets: [{
+            label: '# Of Users',
+            borderWidth: 0,
+            data: [2000, 550, 500],
+            backgroundColor: [
+                '#7477bf',
+                '#78cf82',
+                '#51b6c8'
+            ]
         }]
-    },
+};
+let mobileOptions = {
+    legend: {
+        positions: 'right',
+        labels: {
+            boxWidth: 20,
+            fontStyle: 'bold'
+        }
+    } 
+};
 
-    // Configuration options go here
-    options: {}
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
+
+
+
+//* Messaging Section 
+const userInput = document.getElementById('userField');
+const message = document.getElementById('messageField');
+const send = document.getElementById('send');
+
+//* Ensures Fields are filled out
+send.addEventListener('click', ()=>{
+    if(userInput.value === "" && message.value === ""){
+        alert("Search field and Message field are empty"); //!alert
+    }else if(userInput.value === ""){
+        alert("Input field empty"); //!alert
+    }else if(message.value === ""){
+        alert("Message field Is empty"); //!alert
+    }else{
+        alert(`Message was Successfully sent to ${userInput.value}`); //!alert
+    }
 });
